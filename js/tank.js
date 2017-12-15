3879f9bd881b5688072c30b098d4a43c08028b96
adamredwoods <adamredwoods@users.noreply.github.com>
2017-12-14 16:24:06 -0800

    tank bullets

--- js/tank.js
+++ js/tank.js
@@ -2,10 +2,12 @@ define(["assets"], function(assets) {
    var NUM_TANKS = 10;
    var TANK_RANDOM_MOVE = 50;
    var TANK_SPEED = 3;
+   var AIM_DIST = 600;
 
    var stage, stageHeight;
-   var tank, tankGun, bullet;
+   var tank, tankGun, bullet, bulletLayer;
    var tankList=[];
+   var bulletList = [];
 
    function init(_stage, maxland) {
       stage = _stage;
@@ -23,6 +25,10 @@ define(["assets"], function(assets) {
          tankGun.x = 65;
 
          bullet = new createjs.Bitmap(assets.images.tankBullet);
+
+         //-- bulletLayer is NOT stationary. moves with worldPosition to control all bullets.
+         bulletLayer = new createjs.Container();
+         stage.addChild(bulletLayer);
       }
 
       //--place some tanks randomly
@@ -31,7 +37,7 @@ define(["assets"], function(assets) {
 
       for (var i=0; i<NUM_TANKS; i++) {
          let tt = tank.clone(true);
-         tt.offset = -Math.random()*terrainWidth-stage.canvas.width;
+         tt.offsetX = -Math.random()*terrainWidth-stage.canvas.width;
 
          stage.addChild(tt);
          tankList.push(tt);
@@ -40,7 +46,33 @@ define(["assets"], function(assets) {
          tt.dirX=0;
          tt.getChildAt(0).rotation = Math.random()*100-50;
          tt.gunDir =1;
+         tt.tankAim = false;
+         tt.shootTimer = 0;
       }
+   }
+
+   //-- needs: sourceX in world, sourceY, destX in world, destY
+   function createBullet( sourceX, sourceY, destX, destY, lengthX ) {
+      var bb = bullet.clone();
+      bulletLayer.addChild(bb);
+
+      bb.x = sourceX;
+      bb.y = sourceY;
+
+      //separate tweens to mimic gravity and arcing
+      let dx = Math.abs(sourceX-destX);
+      let dy = Math.abs(sourceY-destY);
+      let t = (dx>dy) ? dx/1000*5000 : dy/1000*5000;
+      createjs.Tween.get(bb).to({x:(destX-lengthX)},t)
+      createjs.Tween.get(bb).to({y:destY}, t*0.5, createjs.Ease.quadOut).call(function () {
+         createjs.Tween.get(bb).to({y:sourceY}, t*0.5, createjs.Ease.quadIn).call(function() {
+            bulletLayer.removeChild(bb);
+         });
+      });
+   }
+
+   function updateBulletLayer(worldPosition) {
+      bulletLayer.x = worldPosition;
    }
 
 
@@ -57,17 +89,50 @@ define(["assets"], function(assets) {
             tankList[i].dirX =0;
          }
 
-         tankList[i].offset += tankList[i].dirX*TANK_SPEED;
-         tankList[i].x = worldPosition + tankList[i].offset;
+         tankList[i].offsetX += tankList[i].dirX*TANK_SPEED;
+         tankList[i].x = worldPosition + tankList[i].offsetX;
          tankList[i].y = stageHeight*0.8;
 
          let rot = tankList[i].getChildAt(0).rotation
-         rot += tankList[i].gunDir*1.0;
-         rot = (rot > 50) ? 50 : rot;
-         rot = (rot < -50) ? -50 : rot;
 
+         let xx = tankList[i].x - ufo.getUFO().x;
+         let yy = tankList[i].y - ufo.getUFO().y;
+         let tankAim = false;
+
+         if (xx < AIM_DIST && xx >-AIM_DIST && yy<AIM_DIST*0.75) {
+            tankAim = true;
+         }
+
+         if (!tankAim) {
+            rot += tankList[i].gunDir*1.0;
+         } else {
+
+            //-- aim at ufo
+            let c = Math.sqrt(xx*xx + yy*yy);
+            //let cc = xx*xx+yy*yy;
+            rot = Math.asin(yy/c)*180/Math.PI-90;
+            if (xx <0 ) rot = -rot;
+         }
+
+         if (rot > 90) {
+            rot=90;
+            tankList[i].gunDir = -tankList[i].gunDir;
+         }
+         if (rot < -90) {
+            rot=-90;
+            tankList[i].gunDir = -tankList[i].gunDir;
+         }
+         tankList[i].getChildAt(0).rotation = rot;
+
+         if (tankAim && tankList[i].shootTimer<createjs.Ticker.getTime()) {
+            //-- shoot a bullet
+            createBullet(tankList[i].offsetX, tankList[i].y, ufo.getUFO().x-worldPosition, ufo.getUFO().y, xx);
+            // console.log(tankList[i].offsetX,ufo.getUFO().x-worldPosition);
+            tankList[i].shootTimer = createjs.Ticker.getTime()+2000;
+         }
       }
 
+      updateBulletLayer(worldPosition);
    }
 
    return {
